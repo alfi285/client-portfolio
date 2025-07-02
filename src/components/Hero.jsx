9fe437890344d2bf2ad2ca05020d3dbbac61a828
ALFIYA A <alfiyazon@gmail.com>
2025-07-03 00:55:28 +0530

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,17 @@ const Hero = () => {
       <h1 className="text-5xl font-bold mb-4">Hi, I'm Alfiya A</h1>
       <p className="text-xl">Full Stack Developer | MERN & Django</p>
       <a href="#projects" className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-xl shadow-md hover:bg-blue-700">View Projects</a>
+    
+    <a
+  href="/resume.pdf"
+  target="_blank"
+  rel="noopener noreferrer"
+  download
+  className="inline-block mt-4 px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+>
+  Download Resume
+</a>
+
     </section>
   )
 }
