6e44f68acea5578c5c9ff58f131f571e357cc4fe
ALFIYA A <alfiyazon@gmail.com>
2025-12-05 20:05:13 +0530

    updation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,9 @@ const About = () => {
       <h2 className="text-3xl font-bold text-center bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">About Me</h2>
       <div className="max-w-4xl mx-auto">
         <p className="text-lg text-center">
-          I am a Python Developer currently working at CricAnalyst Pvt. Ltd., Kinfra Advanced Technology Park, Ramanattukara, Calicut. I hold a Postgraduate degree in Computer Science and have hands on experience in full stack development using the MERN stack and Django. I enjoy building responsive, user focused web applications and continuously exploring new technologies to enhance my skills.</p>
+          A Computer Science postgraduate with hands-on experience in FastAPI, Django, and the MERN stack. Previously worked at CricAnalyst Pvt. Ltd. and currently rejoined Gen Corpus Data Hub as a MERN Stack Developer Intern. Passionate about building clean,
+           scalable applications and continuously learning new technologies.</p>
+      
       </div>
     </section>
   );
